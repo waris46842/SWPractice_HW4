095e401e61335a1f14414282eecdd1030b2ff207
Waris Punnahitanond <waris46842@hotmail.com>
2023-02-11 00:23:34 +0700

    Connect with MongoDB Atlas

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const dotenv = require('dotenv')
-//Route files
-const hospitals = require('./routes/hospitals');
+const connectDB = require('./config/db')
 
 //Load env vars
 dotenv.config({path:'./config/config.env'});
 
+//Connect to database
+connectDB();
+
+//Route files
+const hospitals = require('./routes/hospitals');
+
 const app = express();
+
+//Add body parser
+app.use(express.json());
 
 //Mount routers
 app.use('/api/v1/hospitals', hospitals);
 
 const PORT=process.env.PORT || 3000;
-app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err,promise) => {
+    console.log(`Error: ${err.message}`);
+    //Close server & exit process
+    server.close(() => process.exit(1));
+});
