cf44975d54e5251d7fdcf79b95cc6c3daed754b1
Waris Punnahitanond <waris46842@hotmail.com>
2023-04-08 09:28:50 +0700

    add security and swagger

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv')
 const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 const connectDB = require('./config/db');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
 
 //Load env vars
 dotenv.config({path:'./config/config.env'});
@@ -13,10 +21,10 @@ connectDB();
 const hospitals = require('./routes/hospitals');
 const auth = require('./routes/auth');
 const appointments = require('./routes/appointments');
-const cors = require('cors');
 
 const app = express();
 
+//Enable CORS
 app.use(cors());
 
 //Add body parser
@@ -24,6 +32,45 @@ app.use(express.json());
 
 //Cookie parser
 app.use(cookieParser());
+
+//Sanitize data
+app.use(mongoSanitize());
+
+//Set security headers
+app.use(helmet());
+
+//Prevent XSS attacks
+app.use(xss());
+
+//Rate Limiting
+const limiter = rateLimit({
+    windowsMs:10*60*1000, //10 mins
+    max: 100
+});
+app.use(limiter);
+
+//Prevent http param pollutions
+app.use(hpp());
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Library API',
+            version: '1.0.0',
+            description: 'A simple Express VacQ API'
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000/api/v1'
+            }
+        ],
+    },
+    apis: ['./routes/*.js'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 //Mount routers
 app.use('/api/v1/hospitals', hospitals);
